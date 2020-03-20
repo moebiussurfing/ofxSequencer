e7f7f28ea9a0e22c6f200d4eac820c33c4ca9770
moebiussurfing <moebiussurfing@users.noreply.github.com>
2020-03-19 21:34:10 -0300

    +

--- src/ofxSequencer.cpp
+++ src/ofxSequencer.cpp
@@ -22,37 +22,39 @@ void ofxSequencer::setup(int cols, int beatsPerMinute, int beatsPerBar)
 
     // TODO: BUG: add step 0
     this->cols = cols;// total num cols
-//    this->cols = cols + 1;// total num cols
+    //    this->cols = cols + 1;// total num cols
 
     setBpm(beatsPerMinute, beatsPerBar);
     setMouseActive(true);
     setPosition(0, 0, 24 * cols, 96);
-    
+
     //--
 
-//    // TODO: BUG: add step 0
-//    for (int r = 0; r < NUM_SEQ_NOTES; r++)
-//    {
-//        this->setValue<bool>(r, 0, false);
-//    }
+    //    // TODO: BUG: add step 0
+    //    for (int r = 0; r < NUM_SEQ_NOTES; r++)
+    //    {
+    //        this->setValue<bool>(r, 0, false);
+    //    }
 
     //-
 
     // TODO: maybe should change from vector to array fixed size mode..
     // maybe to use pointers too
 
-//    // resize and erase bools grid. 12 rows and 16 cols
-//    for (int r = 0; r < 12; r++)
-//    {
-//        vector <bool> myBools;
-//        for (int c = 0 ; c < 16; c++)
-//        {
-//            myBools.push_back(false);
-//        }
-//        GRID_values.push_back(myBools);//create row
-//    }
-    
+    //    // resize and erase bools grid. 12 rows and 16 cols
+    //    for (int r = 0; r < 12; r++)
+    //    {
+    //        vector <bool> myBools;
+    //        for (int c = 0 ; c < 16; c++)
+    //        {
+    //            myBools.push_back(false);
+    //        }
+    //        GRID_values.push_back(myBools);//create row
+    //    }
+
     //--
+
+    reset();//to reset beat to -1
 }
 
 void ofxSequencer::setBpm(int beatsPerMinute, int beatsPerBar)
@@ -78,21 +80,22 @@ void ofxSequencer::stop()
 void ofxSequencer::reset()
 {
     bpm.reset();
-//    column = 0;
+    //    column = 0;
 
-// TODO: BUG: add step 0
-    column = - 1;
+    // TODO: BUG: add step 0
+    column = -1;
     for (int r = 0; r < rows.size(); r++)
     {
         rows[r]->update(cols);
-//        rows[r]->;
+        //        rows[r]->;
     }
 
 }
 
 void ofxSequencer::randomize()
 {
-    for (int r=0; r<rows.size(); r++) {
+    for (int r = 0; r < rows.size(); r++)
+    {
         rows[r]->randomize();
     }
     toRedraw = true;
@@ -104,22 +107,22 @@ void ofxSequencer::DEBUG_All_GRID()
 {
     // dump grid matrix values
 
-    ofLogNotice("ofxSequencer")<< "DEBUG_All_GRID";
-    
+    ofLogNotice("ofxSequencer") << "DEBUG_All_GRID";
+
     bool myVal;
     for (int r = 0; r < rows.size(); r++)
     {
         ofLogVerbose("ofxSequencer") << "--- row " << r;
 
         for (int c = 0; c < cols; c++)
-//        for (int c = 0; c < cols-1; c++)
-            {
+            //        for (int c = 0; c < cols-1; c++)
+        {
             myVal = grid[r][c];
-              
+
             ofLogVerbose("ofxSequencer") << "- col " << c << " " << myVal;
         }
     }
-    ofLogVerbose("ofxSequencer") << "-------------------------------------------" ;
+    ofLogVerbose("ofxSequencer") << "-------------------------------------------";
 }
 
 //-------------------------------------------------------------------
@@ -128,24 +131,25 @@ void ofxSequencer::set_GridFromSequencer()
     // get values from internal draw parameters to data grid vector ?
 
     ofLogNotice("ofxSequencer") << "set_GridFromSequencer";
-    
-    for (int r = 0; r < rows.size(); r++) {
+
+    for (int r = 0; r < rows.size(); r++)
+    {
         ofLogVerbose("ofxSequencer") << "--- row " << r;
 
         // TODO: ??
         // save every row columns values on his object.. ??
         rows[r]->store_Row_Values();//working
-        
+
         //-
-        
+
         for (int c = 0; c < cols; c++)
-//        for (int c = 0; c < cols-1; c++)
+            //        for (int c = 0; c < cols-1; c++)
         {
             //ofLogVerbose("ofxSequencer") << "r:" << r << " c:" << c;
-            
+
             bool myVal;
             myVal = rows[r]->grid_Rows[c];//test after move to base
-         
+
             grid[r][c] = myVal;
         }
         ofLogVerbose("ofxSequencer") << "----------------------------";
@@ -166,16 +170,16 @@ void ofxSequencer::set_SequencerFromGrid()
         ofLogVerbose("ofxSequencer") << "row:" << n;
 
         for (int b = 0; b < cols; b++)
-//        for (int b = 0; b < cols-1; b++)
+            //        for (int b = 0; b < cols-1; b++)
         {
             // SEQUENCER CLASS
 
             // set value to sequencer grid squares
 
             bool state = grid[n][b];
-            str += ofToString( state ) + " ";
+            str += ofToString(state) + " ";
 
-            setValue<bool>( n, b, state );
+            setValue<bool>(n, b, state);
 
             //-
         }
@@ -193,19 +197,22 @@ void ofxSequencer::play(void)
 void ofxSequencer::advance()
 {
     column = (column + 1) % cols;
-// TODO: BUG: add step 0
-//    column = (column + 1) % (cols-1);
+    // TODO: BUG: add step 0
+    //    column = (column + 1) % (cols-1);
 
-    if (smooth) {
+    if (smooth)
+    {
         bpmTime = ofGetElapsedTimeMillis();
         cursor = column;
-        for (auto r : rows) {
+        for (auto r : rows)
+        {
             r->update(cursor);
         }
     }
     else
     {
-        for (auto r : rows) {
+        for (auto r : rows)
+        {
             r->update(column);
         }
     }
@@ -217,22 +224,26 @@ void ofxSequencer::advance()
 void ofxSequencer::stepBack()
 {
     column = (column - 1);// % cols;
-    if (column < 0) column = 0;
-    
-    if (smooth) {
+    if (column < 0)
+        column = 0;
+
+    if (smooth)
+    {
         bpmTime = ofGetElapsedTimeMillis();
         cursor = column;
-        for (auto r : rows) {
+        for (auto r : rows)
+        {
             r->update(cursor);
         }
     }
     else
     {
-        for (auto r : rows) {
+        for (auto r : rows)
+        {
             r->update(column);
         }
     }
-    
+
     ofNotifyEvent(beatEvent, column, this);
 }
 
@@ -244,15 +255,19 @@ void ofxSequencer::setMouseActive(bool active)
 {
     if (active)
     {
-        ofAddListener(ofEvents().mousePressed, this, &ofxSequencer::mousePressed);
-        ofAddListener(ofEvents().mouseReleased, this, &ofxSequencer::mouseReleased);
-        ofAddListener(ofEvents().mouseDragged, this, &ofxSequencer::mouseDragged);
+        //ofAddListener(ofEvents().mousePressed, this, &ofxSequencer::mousePressed);
+        //ofAddListener(ofEvents().mouseReleased, this, &ofxSequencer::mouseReleased);
+        //ofAddListener(ofEvents().mouseDragged, this, &ofxSequencer::mouseDragged);
+        ofAddListener(ofEvents().mousePressed, this, &ofxSequencer::mousePressed, OF_EVENT_ORDER_AFTER_APP);
+        ofAddListener(ofEvents().mouseReleased, this, &ofxSequencer::mouseReleased, OF_EVENT_ORDER_AFTER_APP);
+        ofAddListener(ofEvents().mouseDragged, this, &ofxSequencer::mouseDragged, OF_EVENT_ORDER_AFTER_APP);
+
     }
     else
     {
-        ofRemoveListener(ofEvents().mousePressed, this, &ofxSequencer::mousePressed);
-        ofRemoveListener(ofEvents().mouseReleased, this, &ofxSequencer::mouseReleased);
-        ofRemoveListener(ofEvents().mouseDragged, this, &ofxSequencer::mouseDragged);
+        ofRemoveListener(ofEvents().mousePressed, this, &ofxSequencer::mousePressed, OF_EVENT_ORDER_AFTER_APP);
+        ofRemoveListener(ofEvents().mouseReleased, this, &ofxSequencer::mouseReleased, OF_EVENT_ORDER_AFTER_APP);
+        ofRemoveListener(ofEvents().mouseDragged, this, &ofxSequencer::mouseDragged, OF_EVENT_ORDER_AFTER_APP);
     }
 }
 
@@ -262,10 +277,10 @@ void ofxSequencer::mousePressed(ofMouseEventArgs &evt)
     if (seqRect.inside(evt.x, evt.y))
     {
         mCell.set(floor((evt.x - x) / cellWidth),
-                  floor((evt.y - y) / cellHeight));
+            floor((evt.y - y) / cellHeight));
         rows[mCell.y]->mousePressed(mCell.x, evt.x, evt.y);
         draggingCell = true;
-        
+
         ofLogVerbose("ofxSequencer") << "< pressed: mCell x " << mCell.x;
         ofLogVerbose("ofxSequencer") << "< pressed: mCell y " << mCell.y;
     }
@@ -283,13 +298,15 @@ void ofxSequencer::mouseDragged(ofMouseEventArgs &evt)
 
 void ofxSequencer::mouseReleased(ofMouseEventArgs &evt)
 {
-    if (draggingCell && draggingFrames==0) {
+    if (draggingCell && draggingFrames == 0)
+    {
         rows[mCell.y]->mouseReleased(mCell.x);
-        
+
         ofLogVerbose("ofxSequencer") << "< released: mCell x " << mCell.x;
         ofLogVerbose("ofxSequencer") << "< released: mCell y " << mCell.y;
     }
-    else {
+    else
+    {
         draggingFrames = 0;
     }
     draggingCell = false;
@@ -316,7 +333,8 @@ void ofxSequencer::update()
     if (smooth && bpm.isPlaying())
     {
         cursor = column + (float) (ofGetElapsedTimeMillis() - bpmTime) / bpmInterval;
-        for (auto r : rows) {
+        for (auto r : rows)
+        {
             r->update(cursor);
         }
     }
@@ -325,18 +343,19 @@ void ofxSequencer::update()
 //-------------------------------------------------------------------
 void ofxSequencer::draw()
 {
-    if (toRedraw) {
+    if (toRedraw)
+    {
         redraw();
         toRedraw = false;
     }
-    
+
     ofPushMatrix();
     ofPushStyle();
-    
+
     ofTranslate(x, y);
     ofSetColor(255);
     fbo.draw(0, 0, width, height);
-    
+
     ofSetRectMode(OF_RECTMODE_CORNER);
     //ofSetLineWidth(4);
     ofSetLineWidth(2);
@@ -349,7 +368,7 @@ void ofxSequencer::draw()
         ofDrawRectangle(cellWidth * column, 0, cellWidth, height);
         ofSetColor(255, 0, 0, 255 * t);
         ofDrawRectangle(cellWidth * ((column + 1) % cols), 0, cellWidth, height);
-//        ofDrawRectangle(cellWidth * ((column + 1) % (cols-1)), 0, cellWidth, height);
+        //        ofDrawRectangle(cellWidth * ((column + 1) % (cols-1)), 0, cellWidth, height);
 
     }
     else if (column != -1)
@@ -359,7 +378,7 @@ void ofxSequencer::draw()
         //        ofNoFill();
         //        ofSetColor(ofColor::white);
         //        ofDrawRectangle(cellWidth * column, 0, cellWidth, height);
-        
+
         // filled bar with alpha
         ofFill();
         int grey = 255;
@@ -368,12 +387,12 @@ void ofxSequencer::draw()
         c.set(grey);
         ofSetColor(c.r, c.g, c.b, a);
         ofDrawRectangle(cellWidth * column, 0, cellWidth, height);
-        
+
     }
-    
+
     ofPopStyle();
     ofPopMatrix();
-    
+
     //    //debug
     //    ofSetColor(ofColor::white);
     //    ofDrawBitmapString(ofToString(column), 800, 800);
@@ -383,15 +402,15 @@ void ofxSequencer::draw()
 //-------------------------------------------------------------------
 void ofxSequencer::redraw()
 {
-    cellWidth  = (float) width  / cols;
-//    cellWidth  = (float) width  / (cols-1);
+    cellWidth = (float) width / cols;
+    //    cellWidth  = (float) width  / (cols-1);
     cellHeight = (float) height / rows.size();
 
     fbo.begin();
-    
+
     ofPushMatrix();
     ofPushStyle();
-    
+
     ofSetColor(0);
     ofFill();
     ofDrawRectangle(0, 0, width + 120, height);
@@ -399,35 +418,37 @@ void ofxSequencer::redraw()
 
     ofSetRectMode(OF_RECTMODE_CENTER);
     ofTranslate(0.5 * cellWidth, 0.5 * cellHeight);
-    for (int r=0; r<rows.size(); r++)
+    for (int r = 0; r < rows.size(); r++)
     {
         ofSetColor(255);
         for (int c = 0; c < cols; c++)
-//        for (int c = 0; c < cols-1; c++)
+            //        for (int c = 0; c < cols-1; c++)
         {
             rows[r]->draw(c, cellWidth, cellHeight);
             ofTranslate(cellWidth, 0);
         }
         ofTranslate(-cols * cellWidth, cellHeight);
-//        ofSetColor(0, 200, 0);
+        //        ofSetColor(0, 200, 0);
         ofSetColor(ofColor::white);
-        ofDrawBitmapString(rows[r]->getName(), -20 -0.5 * cellWidth + 2, -cellHeight + 8);
+        ofDrawBitmapString(rows[r]->getName(), -20 - 0.5 * cellWidth + 2, -cellHeight + 8);
     }
-    ofTranslate(-0.5*cellWidth, (-0.5-rows.size())*cellHeight);
-    
+    ofTranslate(-0.5 * cellWidth, (-0.5 - rows.size()) * cellHeight);
+
     ofSetColor(100);
     ofSetLineWidth(1);
-    for (int r=1; r<rows.size(); r++) {
+    for (int r = 1; r < rows.size(); r++)
+    {
         ofDrawLine(0, r * cellHeight, width, r * cellHeight);
     }
-    for (int c=1; c<cols; c++) {
-//    for (int c=1; c<cols-1; c++) {
+    for (int c = 1; c < cols; c++)
+    {
+        //    for (int c=1; c<cols-1; c++) {
         ofDrawLine(c * cellWidth, 0, c * cellWidth, height);
     }
-    
+
     ofPopStyle();
     ofPopMatrix();
-    
+
     fbo.end();
 }
 
